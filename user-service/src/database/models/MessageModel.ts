84e4893f5ca97118d435845673c672eb078872c4
akhil <akhil@geekyants.com>
2026-09-04 19:02:27 +0530

    Added message model for mongo db. Updated User model.

--- user-service/src/database/models/MessageModel.ts
+++ user-service/src/database/models/MessageModel.ts
@@ -1,7 +1,34 @@
-import mongoose, {Schema,Document} from "mongoose"
+import mongoose, { Schema, Document } from 'mongoose';
 
-enum Status{
-    NotDelivered = "NotDelivered",
-    Delivered = "Delivered",
-    Seen = "Seen"
+enum Status {
+  NotDelivered = 'NotDelivered',
+  Delivered = 'Delivered',
+  Seen = 'Seen',
 }
+
+interface IMessage extends Document {
+  senderId: string;
+  message: string;
+  status: Status;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const MessageSchema: IMessage = new Schema(
+  {
+    senderId: { type: String, required: true },
+    receiverId: { type: String, required: true },
+    message: { type: String, required: true },
+    status: {
+      type: String,
+      enum: Object.values(Status),
+      default: Status.NotDelivered,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Message = mongoose.model('Message', MessageSchema);
+export default Message;
--- user-service/src/database/models/UserModel.ts
+++ user-service/src/database/models/UserModel.ts
@@ -1,39 +1,41 @@
-import mongoose, { Schema, Document } from "mongoose";
-import validator from "validator";
+import mongoose, { Schema, Document } from 'mongoose';
+import validator from 'validator';
 
-export interface IUser extends Document{
-    name:string;
-    email:string;
-    password:string;
-    createdAt:Date;
-    updatedAt:Date;
+export interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
-const UserSchema : Schema = new Schema({
-    name:{
-        type:String,
-        trim:true,
-        minlength:3,
-        required:[true, "Name must be provided"],
+const UserSchema: Schema = new Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      minlength: 3,
+      required: [true, 'Name must be provided'],
     },
-    email:{
-        type:String,
-        trim:true,
-        lowercase:true,
-        unique:true,
-        required:true,
-        validate:[validator.isEmail,"Please provide a valid email."]
+    email: {
+      type: String,
+      trim: true,
+      lowercase: true,
+      unique: true,
+      required: true,
+      validate: [validator.isEmail, 'Please provide a valid email.'],
     },
-    password:{
-        type:String,
-        trim:true,
-        required:[true, "Password must be provided"],
-        minlength:8
+    password: {
+      type: String,
+      trim: true,
+      required: [true, 'Password must be provided'],
+      minlength: 8,
     },
-},{
-    timestamps:true
-})
+  },
+  {
+    timestamps: true,
+  },
+);
 
-
-const User = mongoose.model<IUser>("User",UserSchema)
+const User = mongoose.model<IUser>('User', UserSchema);
 export default User;
